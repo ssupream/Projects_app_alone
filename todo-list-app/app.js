5474f3a5e50c38c87f746a20d933d9c881c28dbb
ssupraem <alinnsts@gmail.com>
2023-07-27 18:28:49 +0300

    Added side pannel to the App

--- todo-list-app/app.js
+++ todo-list-app/app.js
@@ -1,12 +1,6 @@
 const newTaskBtn = document.querySelector(".js-add-new-task-btn");
 const listContainer = document.querySelector(".list");
 const noTasksText = document.querySelector(".no-tasks");
-const html = `<input type="checkbox" name="checkbox" />
-    <input type="text" placeholder="New Task" />
-    <input type="date" class="js-due-date-input due-date-input" />
-    <button class="details-btn">Details</button>
-    <button class="flag-btn">Flag</button>
-    <button class="del-btn js-del-btn">Delete</button>`;
 
 let listItems = JSON.parse(localStorage.getItem("todoList")) || [];
 
@@ -14,10 +8,11 @@ renderTasksList();
 
 function renderTasksList() {
   let tasksHTML = "";
-  listItems.forEach(() => {
-    tasksHTML += html;
-    listContainer.innerHTML = `<label class="form-control">${tasksHTML}</label>`;
+  listItems.forEach((task, index) => {
+    tasksHTML += createTaskHTML(task, index);
   });
+
+  listContainer.innerHTML = tasksHTML;
 
   if (listItems.length === 0) {
     noTasksText.classList.add("no-tasks");
@@ -28,39 +23,86 @@ function renderTasksList() {
   console.log(listItems);
 }
 
+function createTaskHTML(task, index) {
+  return `
+    <div class="task" data-task-index="${index}">
+      <label class="form-control">
+        <input type="checkbox" name="checkbox" />
+        <input type="text" value="${
+          task.title || ""
+        }" placeholder="New Task" class="js-list-text"/>
+        ${
+          task.flagged
+            ? '<div class="task-flagged js-task-flagged">⚑</div>'
+            : ""
+        }
+        <button class="save-btn" onclick="saveTask(${index})">✅︎</button>      
+        <button class="flag-btn" onclick="flagTask(${index})">⚑</button>
+        <button class="del-btn js-del-btn" onclick="deleteTask(${index})">Delete</button>
+      </label>
+      <input type="date" class="js-due-date-input" value="${
+        task.dueDate || formattedToday
+      }" />
+    </div>
+  `;
+}
+
 function addTask() {
-  const form = document.createElement("label");
-  form.classList.add("form-control");
-  form.innerHTML = html;
-
-  listItems.push(listContainer.appendChild(form));
+  const newTask = {
+    title: "",
+    dueDate: "",
+  };
+  listItems.push(newTask);
   renderTasksList();
-  storeTdoList();
 }
 
-function handleDeleteButtonClick(event) {
-  if (event.target.classList.contains("js-del-btn")) {
-    const button = event.target;
-    const listItem = button.closest("label");
-    const index = listItems.indexOf(listItem);
+function flagTask(index) {
+  const task = listItems[index];
+  task.flagged = !task.flagged;
 
-    if (index !== -1) {
-      listItems.splice(index, 1);
-      listItem.remove();
-    }
-  }
-  if (listItems.length === 0) {
-    noTasksText.classList.add("no-tasks");
-  } else {
-    noTasksText.classList.remove("no-tasks");
-  }
   renderTasksList();
-  storeTdoList();
+  storeTodoList();
 }
 
-function storeTdoList() {
+function deleteTask(index) {
+  listItems.splice(index, 1);
+  renderTasksList();
+  storeTodoList();
+}
+
+function saveTask(index) {
+  const task = listItems[index];
+  const taskTitleInput = (task.title = document.querySelector(
+    `[data-task-index="${index}"] .js-list-text`
+  ));
+  const taskDueDateInput = (task.dueDate = document.querySelector(
+    `[data-task-index="${index}"] .js-due-date-input`
+  ));
+  task.title = taskTitleInput.value || "New task";
+  task.dueDate = taskDueDateInput.value || formattedToday;
+
+  console.log(task);
+
+  storeTodoList();
+  renderTasksList();
+}
+
+const date = new Date();
+let yyyy = date.getFullYear();
+let mm = date.getMonth() + 1;
+let dd = date.getDate();
+
+if (dd < 10) {
+  dd = "0" + dd;
+}
+if (mm < 10) {
+  mm = "0" + mm;
+}
+
+const formattedToday = yyyy + "-" + mm + "-" + dd;
+
+function storeTodoList() {
   localStorage.setItem("todoList", JSON.stringify(listItems));
 }
 
 newTaskBtn.addEventListener("click", addTask);
-listContainer.addEventListener("click", handleDeleteButtonClick);
